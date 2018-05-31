5b8348c1a8477cd4368d4cd50bf076794eedd05c
Valdecy Pereira <valdecypereira@yahoo.com.br>
2018-05-31 11:37:52 -0300

    Update Python-MH-Genetic Algorithm.py

--- Python-MH-Genetic Algorithm.py
+++ Python-MH-Genetic Algorithm.py
@@ -63,12 +63,17 @@ def breeding(population, fitness):
         if (offspring.shape[1] - 1 > 1):
             probability = int.from_bytes(os.urandom(8), byteorder = "big") / ((1 << 64) - 1)
             if (probability >= 0.5):
+                choose_mean = int.from_bytes(os.urandom(8), byteorder = "big") / ((1 << 64) - 1)
                 for j in range(0, offspring.shape[1] - 1):
-                    offspring.iloc[i,j] = (population.iloc[i1, j] + population.iloc[i2, j])/2
+                    if (choose_mean < 0.5):
+                        offspring.iloc[i,j] = (population.iloc[i1, j] + population.iloc[i2, j])/2
+                    else:
+                        rand = int.from_bytes(os.urandom(8), byteorder = "big") / ((1 << 64) - 1)
+                        offspring.iloc[i,j] = rand*population.iloc[i1, j] + (1 - rand)*population.iloc[i2, j]
             else:
-                probability = int.from_bytes(os.urandom(8), byteorder = "big") / ((1 << 64) - 1)
+                choose_initial  = int.from_bytes(os.urandom(8), byteorder = "big") / ((1 << 64) - 1)
                 point_crossover = np.random.randint(offspring.shape[1] - 1, size = 1)[0]
-                if (probability < 0.5):
+                if (choose_initial < 0.5):
                     initial = i1
                     final   = i2
                 else:
@@ -99,19 +104,23 @@ def mutation(offspring, mutation_rate = 0.1, min_values = [-5,-5], max_values = 
         for j in range(0, offspring.shape[1] - 1):
             probability = int.from_bytes(os.urandom(8), byteorder = "big") / ((1 << 64) - 1)
             if (probability < mutation_rate):
-                probability = int.from_bytes(os.urandom(8), byteorder = "big") / ((1 << 64) - 1)
-                if (probability < 0.5):
-                    offspring.iloc[i,j] = offspring.iloc[i,j] + random.uniform(min_values[j], max_values[j])*0.05
-                    if (offspring.iloc[i,j] > max_values[j]):
-                        offspring.iloc[i,j] = max_values[j]
-                    elif (offspring.iloc[i,j] < min_values[j]):
-                        offspring.iloc[i,j] = min_values[j] 
+                choose_mutation = int.from_bytes(os.urandom(8), byteorder = "big") / ((1 << 64) - 1)
+                if (choose_mutation < 0.5):
+                    choose_operation = int.from_bytes(os.urandom(8), byteorder = "big") / ((1 << 64) - 1)
+                    if(choose_operation < 0.5):
+                        offspring.iloc[i,j] = offspring.iloc[i,j] + random.uniform(0, 1)*(max_values[j]- offspring.iloc[i,j])
+                        if (offspring.iloc[i,j] > max_values[j]):
+                            offspring.iloc[i,j] = max_values[j]
+                        elif (offspring.iloc[i,j] < min_values[j]):
+                            offspring.iloc[i,j] = min_values[j] 
+                    else:
+                        offspring.iloc[i,j] = offspring.iloc[i,j] - random.uniform(0, 1)*(offspring.iloc[i,j] - min_values[j])
+                        if (offspring.iloc[i,j] > max_values[j]):
+                            offspring.iloc[i,j] = max_values[j]
+                        elif (offspring.iloc[i,j] < min_values[j]):
+                            offspring.iloc[i,j] = min_values[j]
                 else:
-                    offspring.iloc[i,j] = offspring.iloc[i,j] - random.uniform(min_values[j], max_values[j])*0.05
-                    if (offspring.iloc[i,j] > max_values[j]):
-                        offspring.iloc[i,j] = max_values[j]
-                    elif (offspring.iloc[i,j] < min_values[j]):
-                        offspring.iloc[i,j] = min_values[j]  
+                    offspring.iloc[i,j] = random.uniform(min_values[j], max_values[j])
         offspring.iloc[i,-1] = target_function(offspring.iloc[i,0:offspring.shape[1]-1])
     return offspring
 
